4447b883fb205a11a4f3a669d62595705d0a80ba
berthasaragi <122262846+berthasaragi@users.noreply.github.com>
2023-09-27 12:21:20 +0800

    Add files via upload

---
+++ post_test_1/src/main/java/com/mycompany/post_test_1/Bag.java
@@ -0,0 +1,26 @@
+package com.mycompany.post_test_1;
+
+public class Bag {
+    public String jenis;
+    public String merk;
+    public int harga;
+
+
+    public Bag(String jenis, String merk, int harga) {
+        this.jenis = jenis;
+        this.merk = merk;
+        this.harga = harga;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+    
+    public String getMerk() {
+        return merk;
+    }
+    public int getHarga() {
+        return harga;
+    }
+}
+    
---
+++ post_test_1/src/main/java/com/mycompany/post_test_1/Post_test_1.java
@@ -0,0 +1,18 @@
+package com.mycompany.post_test_1;
+
+import java.util.ArrayList;
+
+public class Post_test_1 {
+    public static void main(String[] args) {
+        ArrayList<Bag> bags = new ArrayList<>();
+        bags.add(new Bag("tote bag", "Dior", 20000000));
+        bags.add(new Bag("selempang", "LV", 35000000));
+        bags.add(new Bag("ransel", "Chanel", 80000000));
+        bags.add(new Bag("waist bag", "Hermes", 50000000));
+        bags.add(new Bag("koper", "Gucci", 45000000));
+        
+        for (Bag tas : bags) {
+            System.out.println("Tas jenis " + tas.getJenis() + " dengan merek " + tas.getMerk() + " seharga " + tas.getHarga() + ".");
+        }
+    }
+}
